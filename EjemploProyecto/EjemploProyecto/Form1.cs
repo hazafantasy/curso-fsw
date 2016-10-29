b4e65c43a7a60df59f4f05db6129ed91b08628e3
hazafantasy <mojica.hazael@gmail.com>
2016-10-29 11:31:19 -0500

    Agregando Hola mundo

--- EjemploProyecto/EjemploProyecto/Form1.cs
+++ EjemploProyecto/EjemploProyecto/Form1.cs
@@ -16,5 +16,10 @@ namespace EjemploProyecto
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello FIME");
+        }
     }
 }
